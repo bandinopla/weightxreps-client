321fa264fd57894d0e2616323c16b85b3fd1d5ef
bandinopla <pablobandinopla@gmail.com>
2024-11-20 16:51:13 +0000

    unused imports---

--- src/componentes/journal/editor-button.js
+++ src/componentes/journal/editor-button.js
@@ -1,29 +1,13 @@
-import { useContext, useEffect, useRef, useState } from "react";
+import { useContext } from "react";
 import { JOwnerContext } from "../../pages/journal-context";
 import AddIcon from '@material-ui/icons/Add';
 import EditIcon from '@material-ui/icons/Edit';
-import { Button, ButtonGroup, Dialog, DialogContent, Grid, Typography } from "@material-ui/core";
-import Backdrop from '@material-ui/core/Backdrop';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
+import { Button } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles';
 import { todayAsYMD } from "../../utils/utils";
-import { SectionTitle } from "../../pages/guest/GuestLandingPage";
 import { useGetSession } from "../../session/session-handler";
-import Alert from "@material-ui/lab/Alert";
-import SaveAltIcon from '@material-ui/icons/SaveAlt';
-import TimerIcon from '@material-ui/icons/Timer';
-import MenuBookIcon from '@material-ui/icons/MenuBook';
-import CloseIcon from '@material-ui/icons/Close';
 
-import { JEditorStopwatch } from "./editor-stopwatch";
-import FileCopyIcon from '@material-ui/icons/FileCopy';
-import { ActionButton } from "../action-button";
-import { BrowserView, MobileOnlyView } from "react-device-detect";
-import { EditorMobileOnly } from "./editor-mobile-only";
-import { useHistory, useLocation } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
   backdrop: {
